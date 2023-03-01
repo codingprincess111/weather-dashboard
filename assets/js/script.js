377427ccc00a6f525e4be322c759921461c41631
codingprincess111 <100244609+codingprincess111@users.noreply.github.com>
2023-03-01 16:50:55 -0600

    added api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,25 @@ var searchForm = document.querySelector ("#search-form")
 var historyContainer = document.querySelector ("#history-container")
 var weatherArea = document.querySelector ("#weather-area")
 var fiveDayForecast = document.querySelector ("five-day-forecast")
+var searchCity = document.querySelector ("#search-city")
 
-function getApi(){
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?"
+var cityTerm = "q="
+var city = "" 
+var apiKeyTerm = "&appid="
+var apiKey= "c2c1687183f47dd81077647bcda4336d"
+var unitTerm = "&units=imperial"
 
+
+
+function getApi(searchUrl){
+    fetch(searchUrl)
+    .then (function (response) {
+        return response.json();   
+    })
+    .then(function(butterfly) {
+        console.log(butterfly);
+    })
 }
 
 function loadHistory (){
@@ -12,9 +28,10 @@ function loadHistory (){
 }
 
 function citySearch (event){
+    city = searchCity.value
+    var searchUrl = apiUrl + cityTerm + city + apiKeyTerm + apiKey + unitTerm
+    getApi(searchUrl);
     event.preventDefault()
-    console.log('hi')
-
 }
 
 searchForm.addEventListener("submit",citySearch);
